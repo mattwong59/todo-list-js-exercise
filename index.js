682cdcd7e86c65fb5b60b3648ed573e46c34a2a4
mattwong59 <mattwong59@gmail.com>
2018-04-10 20:28:24 +0000

    Switched functions to methods in the task object

--- index.js
+++ index.js
@@ -2,19 +2,15 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+    logState: function() {
+      console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+    },
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
-}
-
-//prints out the provided task's details
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
-
-//marks the provided task as completed
-function completeTask(task) {
-  task.complete = true;
 }
 
 // DRIVER CODE BELOW
@@ -23,8 +19,6 @@ const task1 = newTask("Clean Cat Litter", "Task all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
-
-console.log(tasks);
+task1.logState();
+task1.markCompleted();
+task1.logState();
